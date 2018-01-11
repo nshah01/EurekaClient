1a7d3da589f661a5981c4777299d3eee42dfb331
nshah01 <niralishah1494@gmail.com>
2018-01-11 11:52:51 -0500

    Add files via upload

---
+++ src/main/java/com/sample/SampleProjectApplication.java
@@ -0,0 +1,14 @@
+package com.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class SampleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleProjectApplication.class, args);
+	}
+}
